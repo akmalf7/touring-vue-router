5c104f4d6689dbb4ab83a7b834c76f9679934a6c
Akmal Faiz Agiputra <97670902+akmalf7@users.noreply.github.com>
2022-05-17 11:15:15 +0700

    Add files via upload

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+// import { createStore } from 'vuex'
+
+// export default createStore({
+//   state: {},
+//   mutations: {},
+//   actions: {},
+//   modules: {}
+// })
+
+import { reactive } from 'vue'
+
+export default reactive({ flashMessage: '', event: null })
